cc44d8226d211e26efece43828ee97dd9b530a7f
Headshotts <66188700+Headshotts@users.noreply.github.com>
2021-04-07 00:37:40 +0200

    Add files via upload

--- src v2/Jarvis.py
+++ src v2/Jarvis.py
@@ -52,8 +52,27 @@ class Jarvis:
         #print(list)
         return list
 
-    def determinisation(self):
-        transition_table = []
+    def is_lambda_transition(self):
+        lambda_transition = False
+        index = 0
+        while (not lambda_transition) and (index < self.infos[CategoryDesc.NB_STATES]-1):
+            print(index)
+            print(self.infos[CategoryDesc.TRANSITIONS][index])
+            if self.infos[CategoryDesc.TRANSITIONS][index]:
+                for transition, value in self.infos[CategoryDesc.TRANSITIONS][index].items():
+                    if transition == self.infos[CategoryDesc.META]:
+                        lambda_transition = True
+                if not lambda_transition:
+                    index += 1
+            else:
+                index += 1
+        return lambda_transition
+
+    def determinisation_lambda(self):
+
+        transition_table = [] #Récupérer les transitions de table
+        states_list = [] #Récupérer les listes d'états
+
         L = []  # Récupérer les groupes d'états X
         index_L = 1 #Parcourir le groupe d'état
         etats_parcourus = []
@@ -78,23 +97,19 @@ class Jarvis:
                         for state in new_lambdas:
                             if state not in etats_parcourus:
                                 etats_parcourus.append(state)
+
+                        if P not in states_list:
+                            states_list.append(P)
+
                         L.append(new_lambdas)
                         #print("Example " + str(index_L-1) + " : " + str(P) + " " + input_character)
                         #print(new_lambdas)
                         transition_table.append((P, input_character, new_lambdas))
-                        #print(transition_table)
-                        #Création des transitions ici.
-                        #if str(input_character) in self.infos[CategoryDesc.TRANSITIONS][int(P)]:
-                        #    new_transitions[P][str(input_character)].append((str(new_lambdas), str(self.infos[CategoryDesc.META])))
-                        #else:
-                        #    new_transitions[P][str(input_character)] = [(str(new_lambdas), str(self.infos[CategoryDesc.META]))]
 
                 P = L[index_L]
                 index_L += 1
 
             else:
-
-                # Transition vide.
 
                 # Si on a atteint le dernier groupe d'états connus, on vérifie si la déterminisation est finie.
                 if index_L == len(L):
@@ -106,7 +121,62 @@ class Jarvis:
         if len(etats_parcourus) < self.infos[CategoryDesc.NB_STATES]:
             raise Exception("Problème, expected {}, got {} : {}".format(self.infos[CategoryDesc.NB_STATES], len(etats_parcourus), etats_parcourus))
 
-        self.createDotDeterminized(transition_table)
+        self.createDotDeterminized(transition_table, states_list)
+
+    def determinisation_transition(self):
+        transition_table = []  # Récupérer les transitions de table
+        states_list = []  # Récupérer les listes d'états
+
+        L = []  # Récupérer les groupes d'états X
+        index_L = 0  # Parcourir le groupe d'état
+        etats_parcourus = []
+        P = self.infos[CategoryDesc.INIT]
+        # On récupère les états du 1er groupe d'états X
+        for state in P:
+            etats_parcourus.append(state)
+
+        determinisation_finished = False
+        # On récupère les états du 1er groupe d'états X
+
+        # Tant que la déterminisation n'est pas finie, c'est à dire qu'on rencontre de nouveaux lambda-fermetures...
+        while not determinisation_finished:
+            # Afin d'éviter le traitement inutile des lambda-fermetures vides et de refaire un groupe d'états déjà vu.
+            if (P not in L[:(index_L - 1)] and P) or len(L) == 1:
+                # Toutes les lettres de l'alphabet de sortie pour l'appel de transiter.
+                # (Prendre en compte le cas du lambda dans Output (caractère méta ici))
+                for input_character in self.infos[CategoryDesc.INPUT]:
+                    if input_character != self.infos[CategoryDesc.META]:  # Ne pas prendre en compte le caractère lambda.
+                        # On transite avec le groupe d'états X à l'aide de l'output_character
+                        new_lambdas = sorted(self.transiter(P, input_character))
+                        for state in new_lambdas:
+                            if state not in etats_parcourus:
+                                etats_parcourus.append(state)
+
+                        if P not in states_list:
+                            states_list.append(P)
+
+                        L.append(new_lambdas)
+
+                        print("Example " + str(index_L) + " : " + str(P) + " " + input_character)
+                        print(new_lambdas)
+                        transition_table.append((P, input_character, new_lambdas))
+
+                P = L[index_L]
+                index_L += 1
+
+            else:
+
+                # Si on a atteint le dernier groupe d'états connus, on vérifie si la déterminisation est finie.
+                if index_L == len(L):
+                    determinisation_finished = True
+                else:
+                    # On récupère le résultat du lambda fermeture du prochain groupe d'états
+                    P = L[index_L]
+                    index_L += 1
+
+        print(transition_table)
+        self.createDotDeterminized(transition_table, states_list)
+
 
     def createDot(self):
         dot = open("graph.dot", "w")
@@ -120,13 +190,20 @@ class Jarvis:
                 nodeNeighbor = str(value[0][0])
                 inputCharacter = key
                 outputCharacter = str(value[0][1])
-                string = nodeNumber + "->" + nodeNeighbor + " [label=\"" + inputCharacter + "/" + outputCharacter + "\"]\n"
+                string = nodeNumber + "->" + nodeNeighbor + " [label=\"" + inputCharacter
+                if outputCharacter:
+                    string += "/" + outputCharacter
+                string += "\"]\n"
+
                 dot.write(string)
             num += 1
 
+        for final_state in self.infos[CategoryDesc.FINAL]:
+            dot.write(str(final_state) + " [peripheries=2]\n")
         dot.write("}\n")
 
-    def createDotDeterminized(self, transitions):
+
+    def createDotDeterminized(self, transitions, states_list):
         dot = open("determinizedGraph.dot", "w")
         dot.write("digraph G {\n")
 
@@ -145,11 +222,15 @@ class Jarvis:
                 string = "_" + nodeNumber + "->" + "_" + nodeNeighbor + " [label=\"" + inputCharacter + "\"]\n"
                 dot.write(string)
 
+        for final_state in self.infos[CategoryDesc.FINAL]: #Pour chaque état final dans le parsing
+            for states in states_list: #Et pour chaque groupe d'états
+                if final_state in states: #Si l'état final fait partie de l'un des groupes d'états
+                    string = ""
+                    for state in states: #On affiche le groupe d'états dans le .dot et on le met en nomenclature "etat final"
+                        string += "_" + str(state)
+                    dot.write(str(string) + " [peripheries=2]\n")
+
         dot.write("}\n")
-
-
-
-
 
     def use(self, read: str, determinisation: bool=False) -> str:
         for i in read:
@@ -167,7 +248,11 @@ class Jarvis:
 
         print("input = {}".format(read))
         if determinisation:
-            self.determinisation()
+            if self.is_lambda_transition():
+                self.determinisation_lambda()
+            else:
+                self.determinisation_transition()
+
         for letter in read:
             print("{} | '{}'".format(curNode, letter))
 
@@ -183,17 +268,26 @@ class Jarvis:
                 return "can't find a occurrence"
 
         if curNode in self.infos[CategoryDesc.FINAL]:
+            self.createDot()
             return output
         else:
             return "the last node is not final"
 
+
+#C0 : Test de base
+#S0 : Lambda transition
+#S1 : Déterminisation d'un algo sans lambda-transition 1
+#S2 : Déterminisation d'un algo sans lambda-transition 2
+#T0 : Lambda transition
 print("__________________________________________________")
 #try:
-jarvis = Jarvis("../dir/S0.descr")
-print(jarvis.use("ababababa"))
+#jarvis = Jarvis("../dir/S0.descr")
+#print(jarvis.use("ababababa", determinisation=True))
+jarvis = Jarvis("../dir/C0.descr")
+print(jarvis.use("0101101110", determinisation=True))
 #except Exception as err:
     #print("{}{}{}".format("\33[31m", err, "\33[0m"))
-
+'''
 print("__________________________________________________")
 try:
     jarvis.useFileDesc("../dir/S1.descr")
@@ -207,3 +301,4 @@ try:
     print(jarvis.use("010110111"))
 except Exception as err:
     print("{}{}{}".format("\33[31m", err, "\33[0m"))
+'''
